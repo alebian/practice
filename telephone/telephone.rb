90d7b7e2a197bea53b301a1696fe60d69835d75b
alebian <alebezdjian@gmail.com>
2016-03-12 14:56:54 -0300

    Modified telephone

--- telephone/telephone.rb
+++ telephone/telephone.rb
@@ -1,29 +1,25 @@
 class Telephone
-  attr_reader :touch_pad, :number_letters
+  TOUCH_PAD = {
+    A: 2, B: 2, C: 2, D: 3, E: 3, F: 3, G: 4, H: 4, I: 4, J: 5, K: 5, L: 5, M: 6, N: 6, O: 6,
+    P: 7, Q: 7, R: 7, S: 7, T: 8, U: 8, V: 8, W: 9, X: 9, Y: 9, Z: 9
+  }.freeze
 
-  def initialize
-    @touch_pad = {
-      A: 2, B: 2, C: 2, D: 3, E: 3, F: 3, G: 4, H: 4, I: 4, J: 5, K: 5, L: 5, M: 6, N: 6, O: 6,
-      P: 7, Q: 7, R: 7, S: 7, T: 8, U: 8, V: 8, W: 9, X: 9, Y: 9, Z: 9
-    }
-    @number_letters = {
-      2 => 'ABC', 3 => 'DEF', 4 => 'GHI', 5 => 'JKL', 6 => 'MNO', 7 => 'PQRS', 8 => 'TUV',
-      9 => 'WXYZ'
-    }
-  end
+  NUMBER_LETTERS = {
+    2 => 'ABC', 3 => 'DEF', 4 => 'GHI', 5 => 'JKL', 6 => 'MNO', 7 => 'PQRS', 8 => 'TUV',
+    9 => 'WXYZ'
+  }.freeze
 
-  def translate_word(word)
-    letters = word.upcase.split('')
-    answer = ''
-    letters.each do |letter|
-      sym = letter.to_sym
-      answer += touch_pad[sym].to_s if touch_pad.key?(sym)
-      answer += '' unless touch_pad.key?(sym)
+  class << self
+    def to_number(word)
+      letters = word.upcase.split('')
+      letters.inject('') do |answer, letter|
+        answer += TOUCH_PAD[letter.to_sym].to_s if TOUCH_PAD.key?(letter.to_sym)
+        answer
+      end
     end
-    answer
-  end
 
-  def letters(number)
-    number_letters[number.to_i]
+    def letters(number)
+      NUMBER_LETTERS[number.to_i]
+    end
   end
 end
--- test/telephone/test_telephone.rb
+++ test/telephone/test_telephone.rb
@@ -3,9 +3,8 @@ require 'test/unit'
 
 class TestTelephone < Test::Unit::TestCase
   def test_translate_word
-    telephone = Telephone.new
     word = 'alejandro'
     expected = '253526376'
-    assert_equal(expected, telephone.translate_word(word))
+    assert_equal(expected, Telephone.to_number(word))
   end
 end
